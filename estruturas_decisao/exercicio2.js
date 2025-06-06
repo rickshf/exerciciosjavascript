7f2888dacb22b2f459d0d7df8969f412c4bc79f0
Henrique Almeida <henriquealmpin@gmail.com>
2025-06-06 19:47:18 -0300

    exec2

---
+++ estruturas_decisao/exercicio2.js
@@ -0,0 +1,21 @@
+let mes = parseInt(prompt("Digite o número do mês (1 a 12):"));
+
+let nomeMes;
+
+switch (mes) {
+    case 1: nomeMes = "Janeiro"; break;
+    case 2: nomeMes = "Fevereiro"; break;
+    case 3: nomeMes = "Março"; break;
+    case 4: nomeMes = "Abril"; break;
+    case 5: nomeMes = "Maio"; break;
+    case 6: nomeMes = "Junho"; break;
+    case 7: nomeMes = "Julho"; break;
+    case 8: nomeMes = "Agosto"; break;
+    case 9: nomeMes = "Setembro"; break;
+    case 10: nomeMes = "Outubro"; break;
+    case 11: nomeMes = "Novembro"; break;
+    case 12: nomeMes = "Dezembro"; break;
+    default: nomeMes = "Mês inválido";
+}
+
+alert(nomeMes);
