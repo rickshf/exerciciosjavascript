5b93cb2758df05cbf13290358764895f436f8916
Henrique Almeida <henriquealmpin@gmail.com>
2025-06-06 19:56:45 -0300

    arrumando identação

--- estruturas_decisao/exercicio1.js
+++ estruturas_decisao/exercicio1.js
@@ -5,12 +5,12 @@ let c = parseFloat(prompt("Digite o coeficiente c:"));
 let delta = b * b - 4 * a * c;
 
 if (delta < 0) {
-    alert("Não existem raízes reais.");
+  alert("Não existem raízes reais.");
 } else if (delta === 0) {
-    let x = -b / (2 * a);
-    alert(`Existe uma raiz real: x = ${x}`);
+  let x = -b / (2 * a);
+  alert(`Existe uma raiz real: x = ${x}`);
 } else {
-    let x1 = (-b + Math.sqrt(delta)) / (2 * a);
-    let x2 = (-b - Math.sqrt(delta)) / (2 * a);
-    alert(`Existem duas raízes reais: x1 = ${x1}, x2 = ${x2}`);
+  let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+  let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+  alert(`Existem duas raízes reais: x1 = ${x1}, x2 = ${x2}`);
 }
