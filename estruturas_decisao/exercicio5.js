2385c1581b19c59009c4a0414df3172c72968ef1
Henrique Almeida <henriquealmpin@gmail.com>
2025-06-06 19:58:15 -0300

    arrumando identacao

--- estruturas_decisao/exercicio5.js
+++ estruturas_decisao/exercicio5.js
@@ -5,28 +5,28 @@ let operador = prompt("Digite a operação (+, -, *, /):");
 let resultado;
 
 switch (operador) {
-    case "+":
-        resultado = num1 + num2;
-        break;
-    case "-":
-        resultado = num1 - num2;
-        break;
-    case "*":
-        resultado = num1 * num2;
-        break;
-    case "/":
-        if (num2 === 0) {
-            alert("Erro: divisão por zero.");
-            resultado = null;
-        } else {
-            resultado = num1 / num2;
-        }
-        break;
-    default:
-        alert("Operação inválida.");
-        resultado = null;
+  case "+":
+    resultado = num1 + num2;
+    break;
+  case "-":
+    resultado = num1 - num2;
+    break;
+  case "*":
+    resultado = num1 * num2;
+    break;
+  case "/":
+    if (num2 === 0) {
+      alert("Erro: divisão por zero.");
+      resultado = null;
+    } else {
+      resultado = num1 / num2;
+    }
+    break;
+  default:
+    alert("Operação inválida.");
+    resultado = null;
 }
 
 if (resultado !== null) {
-    alert(`Resultado: ${resultado}`);
+  alert(`Resultado: ${resultado}`);
 }
