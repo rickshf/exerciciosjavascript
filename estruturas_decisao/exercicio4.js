343241b98a0ab1a23f99d9095eec2abea39a0506
Henrique Almeida <henriquealmpin@gmail.com>
2025-06-06 19:58:01 -0300

    arrumando identacao

--- estruturas_decisao/exercicio4.js
+++ estruturas_decisao/exercicio4.js
@@ -1,32 +1,34 @@
 let valor = parseFloat(prompt("Digite o valor do produto:"));
-let codigo = parseInt(prompt(
+let codigo = parseInt(
+  prompt(
     "Digite o código de aumento:\n" +
-    "1 - 10%\n" +
-    "3 - 25%\n" +
-    "4 - 30%\n" +
-    "8 - 50%"
-));
+      "1 - 10%\n" +
+      "3 - 25%\n" +
+      "4 - 30%\n" +
+      "8 - 50%"
+  )
+);
 
 let novoValor;
 
 switch (codigo) {
-    case 1:
-        novoValor = valor * 1.10;
-        break;
-    case 3:
-        novoValor = valor * 1.25;
-        break;
-    case 4:
-        novoValor = valor * 1.30;
-        break;
-    case 8:
-        novoValor = valor * 1.50;
-        break;
-    default:
-        alert("Código inválido.");
-        novoValor = null;
+  case 1:
+    novoValor = valor * 1.1;
+    break;
+  case 3:
+    novoValor = valor * 1.25;
+    break;
+  case 4:
+    novoValor = valor * 1.3;
+    break;
+  case 8:
+    novoValor = valor * 1.5;
+    break;
+  default:
+    alert("Código inválido.");
+    novoValor = null;
 }
 
 if (novoValor !== null) {
-    alert(`Novo valor do produto: R$ ${novoValor.toFixed(2)}`);
+  alert(`Novo valor do produto: R$ ${novoValor.toFixed(2)}`);
 }
